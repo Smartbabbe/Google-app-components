69c834edd55f3a2e15bc2f0c00b9e27629f7f72f
ESTHER ISRAEL OKON <estherisrael036@gmail.com>
2023-04-23 22:38:06 +0100

    deleted files

--- component/src/index.js
+++ component/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import GoogleApp from './google app components/GoogleApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
