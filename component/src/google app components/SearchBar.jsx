2da4bff47fbcdcd066f7420c114695647189d2e6
ESTHER ISRAEL OKON <estherisrael036@gmail.com>
2023-04-24 12:03:01 +0100

    changes to the app

--- component/src/google app components/SearchBar.jsx
+++ component/src/google app components/SearchBar.jsx
@@ -3,16 +3,24 @@ import "./searchBar.css"
 import voice from "../Images/voice.png"
 import image from "../Images/add_image.png"
 import search from "../Images/searchlogo.png"
+import { useState } from 'react'
 
 function SearchBar() {
+  const [initial, after] = useState("");
   return (
     <div>
-      <div className='searchBar'>
-        <img className='search' src={search} alt='search icon' />
-        <input type='text' name='searchBar' className='searchTextBar' />
-        <img className='voice' src={voice} alt="voice icon" />
-        <img className='imgss' src={image} alt="file icon" />
+      <div className='divv'>
+        <div className='searchBar'>
+          <img className='search' src={search} alt='search icon' />
+          <input type='text' value={initial} onChange={(e) => {after(e.target.value)}}  name='searchBar' className='searchTextBar' />
+          <img className='voice' src={voice} alt="voice icon" />
+          <img className='imgss' src={image} alt="file icon" />
+        </div>
+        <div>
+        <p>{initial}</p>
+        </div>        
       </div>
+
     </div>
   )
 }
